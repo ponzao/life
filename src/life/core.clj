e9c0386dc90c3ff91640ac428798d492616c9076
Vesa Marttila <vtmarttila@gmail.com>
2011-06-11 17:06:45 +0300

    Some sketching.

--- src/life/core.clj
+++ src/life/core.clj
@@ -8,4 +8,20 @@
       [(+ x xinc)
        (+ y yinc)])))
 
-(declare tick)
+(defn create-grid []
+  (vec (for [x (range 9)]
+         nil)))
+
+(defn create-cell [grid [x y :as cell]]
+  (let [row-length (int (Math/sqrt (count grid)))
+        position (+ x (* row-length y))]
+    (assoc grid position cell)))
+
+(defn survivors [grid]
+  (for [cell grid]
+    (i)))
+
+(defn tick [grid])
+
+(defn generations [initial-grid]
+  (iterate tick initial-grid))
--- test/life/core_test.clj
+++ test/life/core_test.clj
@@ -7,5 +7,12 @@
                                         [0 1]       [2 1]
                                         [0 0] [1 0] [2 0] ]
                                       :in-any-order))
+(fact "grid is 3x3"
+      (life/create-grid) => [nil nil nil
+                             nil nil nil
+                             nil nil nil])
 
-
+(fact "a living cell can be created within a grid"
+      (life/create-cell (life/create-grid) [1 1]) => [nil nil   nil
+                                                      nil [1 1] nil
+                                                      nil nil   nil])
