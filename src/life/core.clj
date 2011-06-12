79776dd184b07343fe063c64d971cf14bc1628a0
Vesa Marttila <vtmarttila@gmail.com>
2011-06-12 11:36:09 +0300

    Dumped testing.

--- src/life/core.clj
+++ src/life/core.clj
@@ -1,27 +1,42 @@
-(ns life.core)
+(ns life.core
+  (:use [clojure.contrib.seq-utils :only [separate]]))
 
-(defn neighbors [[x y]]
-  (let [coordinates [-1 0 1]]
-    (for [xinc coordinates
-          yinc coordinates
-          :when (not= xinc yinc 0)]
-      [(+ x xinc)
-       (+ y yinc)])))
+(defn join-neighbors [[x y] world]
+  (for [xinc [-1 0 1]
+        yinc [-1 0 1]
+        :when (not= xinc yinc 1)]
+    [(+ x xinc) (+ y yinc)]))
 
-(defn create-grid []
-  (vec (for [x (range 9)]
-         nil)))
+(defn neighbors-by-coordinates [world]
+  (for [{coordinates :coordinates status :status} world]
+    [coordinates {:status status}]))
 
-(defn create-cell [grid [x y :as cell]]
-  (let [row-length (int (Math/sqrt (count grid)))
-        position (+ x (* row-length y))]
-    (assoc grid position cell)))
+(defn cells-with-neighbors [world]
+  (into {} (map (fn [[coordinates data]]
+                  [coordinates
+                   (assoc data :neighbors (join-neighbors coordinates (neighbors-by-coordinates world)))])
+                (neighbors-by-coordinates world))))
 
-(defn survivors [grid]
-  (for [cell grid]
-    (i)))
+(def *world* (cells-with-neighbors (create-world)))
 
-(defn tick [grid])
+(defn neighbor-count [{coordinates :coordinates
+                       neighbors   :neighbors   :as cell}]
+  (->> (for [neighbor neighbors]
+         (:status (get *world* neighbor)))
+       (filter (complement nil?))
+       (separate #(= :alive %))
+       (map (juxt first count))
+       (into {})))
 
-(defn generations [initial-grid]
-  (iterate tick initial-grid))
+(defn tick [world]
+  (-> (for [cell world]
+        cell)
+      set))
+
+(defn create-world []
+  #{{:coordinates [0 2] :status :dead} {:coordinates [1 2] :status :dead}  {:coordinates [2 2] :status :dead}
+    {:coordinates [0 1] :status :dead} {:coordinates [1 1] :status :alive} {:coordinates [2 1] :status :dead}
+    {:coordinates [0 0] :status :dead} {:coordinates [1 0] :status :dead}  {:coordinates [2 0] :status :dead}})
+
+(defn generations []
+  (iterate tick (create-world)))
